b07e8f34b760ce3d443271b72c6e2117951a3f94
Jennifer Tarkhov <jtarkhov@gmail.com>
2026-01-08 14:43:04 +0100

    Neu angelegte Tickets werden angezeigt

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+
 const btnNewTicket = document.getElementById("btnNewTicket");
 const modal = document.getElementById("modalOverlay");
 const closeBtn = document.getElementById("closeModal");
@@ -28,8 +30,29 @@ form.addEventListener("submit", function(event) {
     };
     taskList.push(newTask);
     console.log(taskList);
+    createTaskCard(newTask);
     modal.style.display = "none";
     form.reset();
 });
 
 function createTaskCard(task) {
+    const cardList = document.getElementsByClassName("card-list")[0];
+    const card = document.createElement("article");
+    card.classList.add("card");
+    card.innerHTML = `
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+        <select class="PrioritySelect">
+            <option value="high" ${task.priority === "high" ? "selected" : ""}>Hoch</option>
+            <option value="medium" ${task.priority === "medium" ? "selected" : ""}>Mittel</option>
+            <option value="low" ${task.priority === "low" ? "selected" : ""}>Niedrig</option>
+        </select>
+        <p>Datum: ${task.date}</p>
+        <div> 
+            <button type="button" class="btn btn-done"><img src="logo.jpg" alt="erledigt" class="card-user" width="20px"></button>
+            <button type="button" class="btn btn-edit"><img src="bearbeiten.jpeg" alt="erledigt" class="card-user" width="20px"></button>
+        </div>
+    `;
+
+    cardList.appendChild(card);
+}});
